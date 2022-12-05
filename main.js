1e639f1c02b4bd6d09e8ad41ad6ae6ba35d6a225
amruthjithsunil <amruthjithsunil@gmail.com>
2022-12-06 00:51:28 +0530

    locally storing user in object string format

--- main.js
+++ main.js
@@ -2,23 +2,16 @@ let form = document.getElementById('my-form')
 let name = document.getElementById('name');
 let email = document.getElementById('email');
 
-localStorage.setItem('name', '');
-localStorage.setItem('email', '');
-
 form.addEventListener('submit', submitFn);
 
 function submitFn(e){
     e.preventDefault();
-    if(localStorage.getItem('name') == ''){
-        localStorage.setItem('name', name.value); 
-    } else {
-        localStorage.setItem('name', localStorage.getItem('name') + ', ' + name.value);
-    }
-    if(localStorage.getItem('email') == ''){
-        localStorage.setItem('email', email.value); 
-    } else {
-        localStorage.setItem('email', localStorage.getItem('email') + ', ' + email.value);
-    }
+    let user = {
+        name: name.value,
+        email: email.value
+    };
+    let user_ser = JSON.stringify(user);
+    localStorage.setItem('user', user_ser);
     name.value = '';
     email.value = '';
 }
